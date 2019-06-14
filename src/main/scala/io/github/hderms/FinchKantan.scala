36f0045c70cf5063800186d065ed1623a095737f
Dermot Haughey <hderms@gmail.com>
2019-06-13 19:43:29 -0500

    compiles

--- src/main/scala/io/github/hderms/FinchKantan.scala
+++ src/main/scala/io/github/hderms/FinchKantan.scala
@@ -1,3 +1,43 @@
-package scala.io.github.hderms object FinchKantan {
+package io.github.hderms
 
+import io.github.hderms.util.FinchEncoder
+
+object FinchKantan {
+  import java.io.StringWriter
+  import java.nio.charset.Charset
+
+  import com.twitter.io.Buf
+  import io.finch.{Application, Encode}
+  import kantan.csv._
+  import kantan.csv.ops._
+
+  import scala.collection.TraversableOnce
+  import scala.language.higherKinds
+  import FinchEncoder.{csv, Csv}
+
+  implicit def encodeCsv[A, F[_] <: TraversableOnce[_]](
+      implicit e: HeaderEncoder[A]): FinchEncoder.Csv[A, F] =
+    csv((a: F[A], cs: Charset) => {
+
+      produceBufferFromCsv(a, cs, rfc.withHeader)
+    })
+
+  implicit def encodeCsvFromRow[A, F[_] <: TraversableOnce[_]](
+      implicit e: RowEncoder[A]): Csv[A, F] =
+    csv((a: F[A], cs: Charset) => {
+
+      produceBufferFromCsv(a, cs, rfc.withoutHeader)
+    })
+
+  private def produceBufferFromCsv[F[_] <: TraversableOnce[_],
+                                   A: HeaderEncoder](
+      a: F[A],
+      cs: Charset,
+      configuration: CsvConfiguration): Buf = {
+    val string = new StringWriter
+
+    string.writeCsv(a.asInstanceOf[TraversableOnce[A]], configuration)
+
+    Buf.ByteBuffer.Owned(cs.encode(string.toString))
+  }
 }
--- src/main/scala/io/github/hderms/util/FinchEncoder.scala
+++ src/main/scala/io/github/hderms/util/FinchEncoder.scala
@@ -1,3 +1,23 @@
 package io.github.hderms.util
 
-class FinchEncoder {}
+object FinchEncoder {
+  import io.finch.{Application, Encode}
+  import scala.collection.TraversableOnce
+  import scala.language.higherKinds
+  import java.nio.charset.Charset
+  import com.twitter.io.Buf
+
+  type Aux[A, CT <: String] = Encode[A] { type ContentType = CT }
+  type Csv[A, F[_] <: TraversableOnce[_]] = Aux[F[A], Application.Csv]
+
+  def instance[A, CT <: String](fn: (A, Charset) => Buf): Aux[A, CT] =
+    new Encode[A] {
+      type ContentType = CT
+
+      def apply(a: A, cs: Charset): Buf = fn(a, cs)
+    }
+
+  def csv[A, F[_] <: TraversableOnce[_]](
+      fn: (F[A], Charset) => Buf): Csv[A, F] =
+    instance[F[A], Application.Csv](fn)
+}
